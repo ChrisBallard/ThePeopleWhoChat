abdbfe5e08164c4f9fd53f4167ac81e1e05c6ceb
ChrisBallard <worldoflard@googlemail.com>
2014-06-12 23:10:47 +0100

    ignore the right test this time...

--- ThePeopleWhoChat.Core.UnitTest/Spikes.fs
+++ ThePeopleWhoChat.Core.UnitTest/Spikes.fs
@@ -15,6 +15,7 @@
         member this.``spikes to be enabled & run as and when required``() = ()
 
         [<Test>]
+        [<Ignore>]
         member this.``ravenDb will respect millisecond time differences in orderBy``() =
             // note this spike requires the database to be empty. run deleteall in the AdminTool before running this
             let url = ConfigurationManager.AppSettings.[Consts.DbUrlSettingKey]
